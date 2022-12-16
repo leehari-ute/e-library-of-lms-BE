178c7d7d013a6a5fe81d9a37f9e7353536e647b5
hle208 <haingoc.le@dxc.com>
2022-12-16 22:21:55 +0700

    update cors

--- src/app.js
+++ src/app.js
@@ -38,6 +38,11 @@ app.use(mongoSanitize());
 app.use(compression());
 
 // enable cors
+app.all('/', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
 app.use(
   cors({
     origin: '*',
